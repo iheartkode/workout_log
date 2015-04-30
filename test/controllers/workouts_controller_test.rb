9f96ac59d59280bffffdb7004020dcf423c91d37
iheartkode <markhjr@outlook.com>
2015-04-30 12:12:10 -0700

    Added index view test and added a fixtures

--- test/controllers/workouts_controller_test.rb
+++ test/controllers/workouts_controller_test.rb
@@ -6,4 +6,10 @@ class WorkoutsControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:workouts)
   end
+  
+  test "index should render correct template and layout" do
+    get :index
+    assert_template :index
+    assert_template layout: "layouts/application"
+  end
 end
