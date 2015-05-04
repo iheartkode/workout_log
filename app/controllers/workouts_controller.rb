0788182ce9c09e1f036a772c0766905a8f26472a
iheartkode <markhjr@outlook.com>
2015-05-04 14:02:26 -0700

    Making heroku ready

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,18 +2,22 @@ class WorkoutsController < ApplicationController
 
   before_action :set_workout, only: [:show, :edit, :update, :destroy]
 
+  #Get all workouts created_at dates descending
   def index
     @workouts = Workout.all.order("created_at desc")
   end
 
+  #The action that shows the workout
   def show
 
   end
 
+  #The action that creates a new workout and directs to the new view
   def new
     @workout = Workout.new
   end
 
+  #The action that actually saves the workout
   def create
     @workout = Workout.new(workout_params)
     if @workout.save
@@ -24,10 +28,12 @@ class WorkoutsController < ApplicationController
     end
   end
 
+  #The action that grabs the workout and show the edit view
   def edit
     @workout = Workout.find(params[:id])
   end
 
+  #The action that takes the parameters and updates it in the db
   def update
     @workout = Workout.find(params[:id])
     @workout.update(workout_params)
@@ -35,12 +41,14 @@ class WorkoutsController < ApplicationController
     redirect_to root_path
   end
 
+  #Destroys the workout when you click delete
   def destroy
     @workout.destroy
     flash[:success] = "Workout was successfully deleted"
     redirect_to root_path
   end
 
+ #Keep the params and such private and secure
  private
   def set_workout
       @workout = Workout.find(params[:id])
