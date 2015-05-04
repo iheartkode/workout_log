5f3a92a72e44b90ff45911f90490dd4faf74db54
iheartkode <markhjr@outlook.com>
2015-05-04 10:49:57 -0700

    Added destroy action and a link_to the delete

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -35,6 +35,12 @@ class WorkoutsController < ApplicationController
     redirect_to root_path
   end
 
+  def destroy
+    @workout.destroy
+    flash[:success] = "Workout was successfully deleted"
+    redirect_to root_path
+  end
+
  private
   def set_workout
       @workout = Workout.find(params[:id])
